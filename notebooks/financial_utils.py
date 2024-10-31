75caeb0e88601db12ebd9befbf7e58865d14eb93
Tess Breton <tessbreton@MacBook-Air-de-Tess.local>
2024-10-31 11:54:34 +0100

    update

--- notebooks/financial_utils.py
+++ notebooks/financial_utils.py
@@ -1,5 +1,3 @@
-import pandas as pd
-import os
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.dates as mdates
@@ -18,10 +16,8 @@ def plot_individual_stock(df, name='AMAZON', color='black', year_ticks=True):
 
 
 def legend_unique_labels():
-    # Get existing handles and labels
     handles, labels = plt.gca().get_legend_handles_labels()
 
-    # Filter out duplicate legend items
     unique_handles = []
     unique_labels = []
     for handle, label in zip(handles, labels):
@@ -29,11 +25,9 @@ def legend_unique_labels():
             unique_handles.append(handle)
             unique_labels.append(label)
 
-    # Create legend with unique items
     plt.legend(unique_handles, unique_labels)
 
 def get_predicted_prices(sims,nb_sim,prev_close):
-    # Calculate predicted prices based on predicted returns
     predicted_returns = sims.values[-1, :nb_sim].T
     predicted_prices = [prev_close]
 
